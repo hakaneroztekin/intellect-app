e3527e90d2e7c852fd13c70b668d91623d820a97
hakaneroztekin <eroztekin@itu.edu.tr>
2018-10-18 09:04:19 +0300

    add basic design to signin, signup and mylists pages and project planning

--- server.py
+++ server.py
@@ -1,3 +1,16 @@
+# Intellect app
+#
+# Upcoming Project Works:
+# Views
+# Database connection testing
+# Database methods (INSERT INTO etc.)
+# Signup form (with DB actions)
+# Signin form
+# Authentication
+# Logout action
+# Movies, Musics form and its actions (adding, updating musics etc.)
+# Report, Presentation
+
 from flask import Flask, render_template
 
 
@@ -8,6 +21,20 @@ app = Flask(__name__)
 def home_page():
     return render_template("homepage.html")
 
+
+@app.route('/mylists')
+def mylists_page():
+    return render_template("mylists.html")
+
+
+@app.route('/signin')
+def signin_page():
+    return render_template("signin.html")
+
+
+@app.route('/signup')
+def signup_page():
+    return render_template("signup.html")
 
 if __name__ == "__main__":
     app.run()
